3e8940b3c8a657eac648f749423f09c4c56b9ffc
NadaAly56 <106588334+NadaAly56@users.noreply.github.com>
2022-05-31 11:37:35 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Web.UI.WebControls;
+using System.Collections;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	public Class1()
+	{
+		//
+		// TODO: Add constructor logic here
+		//  
+	}
+    public static string Aya(string seg)
+    {
+        //ArrayList list = new ArrayList();
+        //list.Add(seg);
+        string newText = "";
+        newText = seg.Replace("َ","");
+        newText = seg.Replace("ً", "");
+        newText = seg.Replace("ُ", "");
+        newText = seg.Replace("ٌ", "");
+        newText = seg.Replace("ِ", "");
+        newText = seg.Replace("ٍ", "");
+        newText = seg.Replace("~", "");
+        newText = seg.Replace("ْ", ""); ;
+        newText = seg.Replace("ّ", "");
+      
+        //list.Add(newText);
+       
+        return newText;
+     
+    }
+}
